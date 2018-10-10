acdd61e70d6f304c782c64f85c30596d76aca9d5
kangya <fjiankang@czbapp.com>
2018-10-10 16:27:50 +0800

    【0.0.7】添加【内存监控】

--- FPSDisplay/ViewController.m
+++ FPSDisplay/ViewController.m
@@ -21,10 +21,48 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-
+    
+//    [self makeCFRunloop];
+    
     [FPSViewManager show];
 }
 
+-(void)makeCFRunloop {
+    CFRunLoopObserverRef observer = CFRunLoopObserverCreateWithHandler(CFAllocatorGetDefault(), kCFRunLoopAllActivities, YES, 0, ^(CFRunLoopObserverRef observer, CFRunLoopActivity activity) {
+        switch (activity) {
+            case kCFRunLoopEntry:
+                NSLog(@"Runloop 进入");
+                break;
+            case kCFRunLoopBeforeTimers:
+                NSLog(@"Runloop 要处理timers了");
+                break;
+            case kCFRunLoopBeforeSources:
+                NSLog(@"Runloop 要处理sources了");
+                break;
+            case kCFRunLoopBeforeWaiting:
+                NSLog(@"Runloop 要休息了");
+                break;
+            case kCFRunLoopAfterWaiting:
+                NSLog(@"Runloop 醒来了");
+                break;
+            case kCFRunLoopExit:
+                NSLog(@"Runloop 退出了");
+                break;
+            default:
+                break;
+        }
+    });
+    
+    CFRunLoopAddObserver(CFRunLoopGetCurrent(), observer, kCFRunLoopDefaultMode);
+    
+    /*
+     CF的内存管理（Core Foundation）
+     凡是带有Create、Copy、Retain等字眼的函数，创建出来的对象，都需要在最后做一次release
+     GCD本来在iOS6.0之前也是需要我们释放的，6.0之后GCD已经纳入到了ARC中，所以我们不需要管了
+     */
+    CFRelease(observer);
+    
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
--- FPSView.podspec
+++ FPSView.podspec
@@ -9,7 +9,7 @@
 Pod::Spec.new do |s|
 
   s.name         = "FPSView"
-  s.version      = "0.0.6"
+  s.version      = "0.0.7"
   s.summary      = "Show the fps of iOS application."
 
   s.homepage     = "https://github.com/fengjiankang/FPSView.git"
