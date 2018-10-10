bc4638854c20502862ca139bbfd8b8415b5d2e05
kangya <fjiankang@czbapp.com>
2018-10-10 16:54:08 +0800

    【0.0.9】

--- FPSDisplay/View/FPSView.m
+++ FPSDisplay/View/FPSView.m
@@ -7,6 +7,8 @@
 //
 
 #import "FPSView.h"
+#include <sys/sysctl.h>
+#import <mach/mach.h>
 
 @interface FPSView()
 
@@ -19,7 +21,11 @@
 // last time
 @property (assign, nonatomic) CFTimeInterval lastTime;
 
+// fpsLabel
 @property (strong, nonatomic) UILabel* fpsLabel;
+
+// memoryLabel
+@property (strong, nonatomic) UILabel* memoryLabel;
 
 @end
 
@@ -39,6 +45,8 @@
 -(void)setup {
     [self initCADisplaylink];
     [self initCountText];
+    [self initMemoryText];
+    
 }
 
 -(void)initCADisplaylink {
@@ -71,16 +79,55 @@
 // update fps text
 -(void)updateDisplayLabelText:(float)count {
     self.fpsLabel.text = [NSString stringWithFormat:@"fps: %@", [NSNumber numberWithFloat:count]];
+    
+    [self calculateMemorySize];
 }
 
 -(UILabel *)fpsLabel {
     if (!_fpsLabel) {
-        _fpsLabel = [[UILabel alloc]initWithFrame:self.frame];
+        CGRect frame = CGRectMake(0, 0, 100, 50);
+        _fpsLabel = [[UILabel alloc]initWithFrame:frame];
         _fpsLabel.textAlignment = NSTextAlignmentCenter;
         _fpsLabel.textColor = [UIColor whiteColor];
         _fpsLabel.font = [UIFont systemFontOfSize: 20];
     }
     return _fpsLabel;
+}
+
+-(UILabel *)memoryLabel {
+    if (!_memoryLabel) {
+        CGRect frame = CGRectMake(0, 50, 100, 50);
+        _memoryLabel = [[UILabel alloc]initWithFrame:frame];
+        _memoryLabel = [[UILabel alloc]initWithFrame:frame];
+        _memoryLabel.textAlignment = NSTextAlignmentCenter;
+        _memoryLabel.textColor = [UIColor whiteColor];
+        _memoryLabel.font = [UIFont systemFontOfSize: 14];
+    }
+    return _memoryLabel;
+}
+
+-(void)initMemoryText {
+    [self addSubview:self.memoryLabel];
+}
+
+-(void)calculateMemorySize {
+    int64_t size = [self memoryUsage] / 1024 / 1024;
+    self.memoryLabel.text = [NSString stringWithFormat:@"内存：%lldM", size];
+    NSLog(@"size is %lld", size);
+}
+
+- (int64_t)memoryUsage {
+    int64_t memoryUsageInByte = 0;
+    task_vm_info_data_t vmInfo;
+    mach_msg_type_number_t count = TASK_VM_INFO_COUNT;
+    kern_return_t kernelReturn = task_info(mach_task_self(), TASK_VM_INFO, (task_info_t) &vmInfo, &count);
+    if(kernelReturn == KERN_SUCCESS) {
+        memoryUsageInByte = (int64_t) vmInfo.phys_footprint;
+        NSLog(@"Memory in use (in bytes): %lld", memoryUsageInByte);
+    } else {
+        NSLog(@"Error with task_info(): %s", mach_error_string(kernelReturn));
+    }
+    return memoryUsageInByte;
 }
 
 @end
