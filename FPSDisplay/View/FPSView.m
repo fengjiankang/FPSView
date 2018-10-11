f8952192074eef064b8cb5ac381fcb26f2225056
kangya <fjiankang@czbapp.com>
2018-10-11 10:44:02 +0800

    【Add】添加【主线程卡顿监控】，暂无法打印堆栈信息，待优化

--- FPSDisplay/View/FPSView.m
+++ FPSDisplay/View/FPSView.m
@@ -9,6 +9,8 @@
 #import "FPSView.h"
 #include <sys/sysctl.h>
 #import <mach/mach.h>
+#include <libkern/OSAtomic.h>
+#include <execinfo.h>
 
 @interface FPSView()
 
@@ -27,13 +29,23 @@
 // memoryLabel
 @property (strong, nonatomic) UILabel* memoryLabel;
 
-CFRunLoopObserverRef _observer;
+@end
 
+@interface FPSView()
+{
+    CFRunLoopObserverRef _observer;
+    double _lastRecordTime;
+    NSMutableArray *_backtrace;
+}
 @end
 
 @implementation FPSView
 
 static double _waitStartTime;
+
+static NSString * MonitorThreadName = @"MonitorThreadName";
+
+static double StumbleCritical = 2.0;
 
 -(instancetype)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
@@ -57,6 +69,7 @@ static double _waitStartTime;
 
 #pragma mark runloop observer
 -(void)addMainThreadObserver {
+
     dispatch_async(dispatch_get_main_queue(), ^{
         @autoreleasepool {
             NSRunLoop *loop = [NSRunLoop currentRunLoop];
@@ -72,11 +85,11 @@ static double _waitStartTime;
             // 设置回调
             // observer's context
             
-            _observer = CFRunLoopObserverCreate(kCFAllocatorDefault, kCFRunLoopAllActivities, YES, 0, &myRunLoopObserver, &context);
+            self->_observer = CFRunLoopObserverCreate(kCFAllocatorDefault, kCFRunLoopAllActivities, YES, 0, &myRunLoopObserver, &context);
             
-            if (_observer) {
+            if (self->_observer) {
                 CFRunLoopRef cfRunloop = [loop getCFRunLoop];
-                CFRunLoopAddObserver(cfRunloop, _observer, kCFRunLoopDefaultMode);
+                CFRunLoopAddObserver(cfRunloop, self->_observer, kCFRunLoopDefaultMode);
             }
         }
     });
@@ -109,8 +122,79 @@ void myRunLoopObserver(CFRunLoopObserverRef observer, CFRunLoopActivity activity
     }
 }
 
+#pragma mark addTimerThreadAndObserver
+
+// monitor thread
+-(NSThread *)timerThread {
+    static NSThread *_timerThread = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        _timerThread = [[NSThread alloc]initWithTarget:self selector:@selector(networkRequestThreadEntryPoint:) object:nil];
+        [_timerThread start];
+    });
+    return _timerThread;
+}
+
+// add the mach port to keep the loop stay and prevent the thread from exiting
+-(void)networkRequestThreadEntryPoint:(id)__unused object {
+    @autoreleasepool {
+        [[NSThread currentThread] setName: MonitorThreadName];
+        NSRunLoop *loop = [NSRunLoop currentRunLoop];
+        [loop addPort:[NSMachPort port] forMode:NSRunLoopCommonModes];
+        [loop run];
+    }
+}
+
+// add timer thread
+-(void)addTimerThreadAndObserver {
+    NSThread *thread = [self timerThread];
+    [self performSelector:@selector(addMonitorTimer) onThread:thread withObject:nil waitUntilDone:YES];
+}
+
+// add monitor timer
+-(void)addMonitorTimer {
+    // timer can retain the self, and self retain the runloop, runloop retain the timer, can result the circle retain, will optimize soon.
+    // optimize method: 1: block, 2: NSProxy
+    NSTimer *timer = [NSTimer timerWithTimeInterval:0.5 target:self selector:@selector(timerFired:) userInfo:nil repeats:YES];
+    [[NSRunLoop currentRunLoop] addTimer:timer forMode:NSDefaultRunLoopMode];
+}
+
+// monitor timer call out
+-(void)timerFired:(NSTimer *)timer {
+    if (_waitStartTime < 1) return;
+    double currentTime = [[NSDate date] timeIntervalSince1970];
+    double timeDiff = currentTime - _waitStartTime;
+    if (timeDiff > StumbleCritical) {
+        if (_lastRecordTime - _waitStartTime < 0.001 && _lastRecordTime != 0){
+            NSLog(@"last time no :%f %f",timeDiff, _waitStartTime);
+            return;
+        }
+        [self logStack];
+    }
+}
+
+
+// log stack stumble informatotion
+-(void)logStack {
+    NSLog(@"主线程发生了卡顿");
+  
+    void* callstack[128];
+    int frames = backtrace(callstack, 128);
+    char **strs = backtrace_symbols(callstack, frames);
+    int i;
+    _backtrace = [NSMutableArray arrayWithCapacity:frames];
+    for ( i = 0 ; i < frames ; i++ ){
+        [_backtrace addObject:[NSString stringWithUTF8String:strs[i]]];
+    }
+    free(strs);
+}
+
+// start monitor
 -(void)startMonitor {
+    // add main thread runloop observer
     [self addMainThreadObserver];
+    // add  background monitor thread
+    [self addTimerThreadAndObserver];
 }
 
 -(void)initCADisplaylink {
